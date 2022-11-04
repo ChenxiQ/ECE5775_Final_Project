84ce4111a29a1d7ecae782ef88e5681be4fd1404
cq53 <cq53@cornell.edu>
2022-11-03 22:53:48 -0400

    Allow create dataset with different size

--- ecelinux/data/create_dataset.py
+++ ecelinux/data/create_dataset.py
@@ -1,12 +1,14 @@
 from mnist import MNIST
 
+SIZE = 200
+
 digits, labels = MNIST('./MNIST').load_training()
 
 with open("./image.dat", "w") as image_file:
-    for i in range(200):
+    for i in range(SIZE):
         tmp = ", ".join(str(bit) for bit in digits[i]) + "\n"
         image_file.writelines(tmp)
 
 with open("./label.dat", "w") as label_file:
-    for i in range(200):
+    for i in range(SIZE):
         label_file.writelines(str(labels[i]) + "\n")
